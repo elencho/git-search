f07b372c8fd34dd35b030f60c8c5fed6e69b3183
elencho <e.bochoradze@gmail.com>
2022-03-31 18:02:39 +0400

    feat: default card style

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,15 @@ const styles = {
     margin: "15px 10px",
     padding: 0,
     borderRadius: "16px",
-    backgroundColor: "red",
+    backgroundColor: "#161B22",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    //borderColor: "#30363D",
+    borderColor: "#30363D",
+    borderWidth: "2px",
+    borderStyle: "solid",
   },
   small: {
     gridRowEnd: "span 26",
@@ -15,6 +23,14 @@ const styles = {
   },
   large: {
     gridRowEnd: "span 45",
+  },
+  iamge: {
+    width: "100px",
+    heigth: "100px",
+    borderRadius: "50%",
+  },
+  text: {
+    color: "#C8D1D9",
   },
 };
 
@@ -26,7 +42,13 @@ const Card = (props) => {
         ...styles.card,
         ...styles[size],
       }}
-    ></div>
+    >
+      <img
+        src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+        style={styles.iamge}
+      />
+      {/* <p style={styles.text}>asd</p> */}
+    </div>
   );
 };
 
